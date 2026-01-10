fe69b7a1da612c6715b882ef45df66206ccd2c9b
Kabir Shuvo <kabirshuvo19@gmail.com>
2026-01-11 01:15:07 +0600

    monalo-app-build-test

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
-// @ts-nocheck
 import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __prisma: PrismaClient | undefined
+}
 
 export const prisma = globalThis.__prisma || new PrismaClient()
 if (!globalThis.__prisma) globalThis.__prisma = prisma
