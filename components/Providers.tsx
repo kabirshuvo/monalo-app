9b74b595cb30e065a95cf86e3ffb7cabbc52033d
Kabir Shuvo <kabirshuvo19@gmail.com>
2026-01-17 12:34:38 +0600

    session provider updated

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,13 +2,16 @@
 import React from 'react'
 import { CartProvider } from '@/contexts/CartContext'
 import { ToastProvider } from '@/components/ui'
+import { SessionProvider } from 'next-auth/react'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ToastProvider>
-      <CartProvider>
-        {children}
-      </CartProvider>
-    </ToastProvider>
+    <SessionProvider>
+      <ToastProvider>
+        <CartProvider>
+          {children}
+        </CartProvider>
+      </ToastProvider>
+    </SessionProvider>
   )
 }
